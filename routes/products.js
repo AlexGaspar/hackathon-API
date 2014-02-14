37b0129a29ba0471d70db6c5e4f32de3caf190fb
Alex <al.gaspar@gmail.com>
2014-02-14 12:55:26 +0100

    Viiiiiiickinnnnnnnng!

--- routes/products.js
+++ routes/products.js
@@ -5,10 +5,13 @@ var Q = require('q');
 
 module.exports = function(server, APIVERSION) {
   var Products = server.models.products;
+  var log = server.helper.log;
 
-
+  // if needed move this in helpers
   var find = function(query) {
     var deferred = Q.defer();
+
+    log.info('find -- Query : ', query);
 
     Products.find(query, function(err, docs) {
       if(err) deferred.reject(err);
@@ -18,12 +21,20 @@ module.exports = function(server, APIVERSION) {
     return deferred.promise;
   }
 
+/**
+ * [getByQuery description]
+ * @param  {[type]}   query [description]
+ * @param  {[type]}   res   [description]
+ * @param  {Function} next  [description]
+ * @return {[type]}         [description]
+ */
+  var getByQuery = function(query, res, next) {
+   find(query).then(function(docs){
+      var response = {
+        data: docs
+      };
 
-  var getAll = function(req, res, next) {
-    var query = {};
-
-    find(query).then(function(docs){
-      res.send(docs);
+      res.send(response);
     }).fail(function(err) {
       res.send(500);
     }).fin(function () {
@@ -31,15 +42,47 @@ module.exports = function(server, APIVERSION) {
     });
   };
 
+  /**
+   * [getAll description]
+   * @param  {[type]}   req  [description]
+   * @param  {[type]}   res  [description]
+   * @param  {Function} next [description]
+   * @return {[type]}        [description]
+   */
+  var getAll = function(req, res, next) {
+    var query = {};
+
+    getByQuery(query, res, next);
+  };
+
+  /**
+   * [getByName description]
+   * @param  {[type]}   req  [description]
+   * @param  {[type]}   res  [description]
+   * @param  {Function} next [description]
+   * @return {[type]}        [description]
+   */
   var getByName = function(req, res, next) {
+    var query = {
+      name: req.params.name
+    };
 
+    getByQuery(query, res, next);
+  };
 
-  }
+  var getAByBarCode = function(req, res, next) {
+    var query = {
+      barcode: req.params.barcode
+    };
+
+    getByQuery(query, res, next);
+  };
+
 
 
   server.get(APIVERSION + '/products', getAll);
   server.get(APIVERSION + '/products/name/:name', getByName);
-  // server.get(APIVERSION + '/products/barcode/:barcode', getAByBarCode;
+  server.get(APIVERSION + '/products/barcode/:barcode', getAByBarCode);
 
   return server;
 };
